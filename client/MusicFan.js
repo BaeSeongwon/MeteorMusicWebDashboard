a20bc42abc458168a466b43a332ac533b3ae09e6
배성원/동서대학교 <seongwon9179@gmail.com>
2016-06-12 05:47:17 +0900

    graph half complete update

--- client/MusicFan.js
+++ client/MusicFan.js
@@ -1,15 +1,19 @@
 Session.setDefault("sideMenue","glyphicon glyphicon-chevron-left");
 Session.setDefault("sideHeight",window.outerHeight - 70);
 Session.setDefault("MelonChartTitle","실시간");
+Session.setDefault('choiceImg');
 
 //멜론 API 인증
-$(document).ready(function(){
+Meteor.startup(function(){
   PlanetX.init({
     appkey : '851b0a60-1a2a-3b84-afb6-f60d9b6bb60e',
     client_id : "8cd1a504-afc1-3e13-a8f2-9201e35637f6",
     scope : "melon",
     savingToken : true
   });
+
+  MelonCall(callMelonAddress.realTimeMusic,callMelonAddress.realTimeTodayMusicChart);
+  Session.set("MelonChartTitle","실시간");
 });
 
 Template.SideMenue.helpers({
@@ -37,6 +41,49 @@ Template.SideMenue.events({
       $("#profileDropdown").fadeOut(function(){return profileDropdown.style.left = 70 + 'px';});
       profileObject.dropdown = "invisible";
     };
+  }
+});
+
+Template.TopHeader.events({
+  'keyup #searchInput': function (event) {
+    if (event.which === 13) {
+      Session.set("singer",$("#searchInput").val());
+      Session.set("thumNail", "");
+      var url = daumService.urlMake($("#searchInput").val(), daumService.daumApiKey);
+      daumService.daumAjax(url);
+
+      //수정 해야됨
+      _promise(daumService.daumImage).then(function(data){
+        searchImg(data);
+      }, function(error){
+        console.log(error);
+      });
+
+      $(".popupBackground").fadeIn();
+      $(".imagePopup").fadeIn();
+    }
+  }
+});
+
+Template.MainBoard.helpers({
+  title : function(){
+    return Session.get("singer");
+  },
+  Img : function(){
+    //console.log(Session.get("thumNail"));
+    return Session.get("thumNail");
+  }
+});
+
+Template.MainBoard.events({
+  'click #delPopup' : function() {
+    $(".popupBackground").fadeOut();
+    $(".imagePopup").fadeOut();
+  },
+  'click .imgBox' : function(){
+    $(".popupBackground").fadeOut();
+    $(".imagePopup").fadeOut();
+    stringImgEqualse($(event.target)[0].currentSrc);
   }
 });
 
@@ -89,9 +136,36 @@ Template.MainMusicChart.events({
     }
   },
   'click .pageTopButton' : function() {
-    $('#musicContainer').animate({scrollTop:0},450);
+    $('#musicContainer').animate({scrollTop: 0}, 500);
   }
 });
+
+Template.MainPicture.helpers({
+  src : function(){
+    console.log(Session.get('choiceImg'));
+    return Session.get('choiceImg');
+  }
+});
+
+function stringImgEqualse(data){
+  var imgData = Session.get('thumNail');
+  console.log(imgData);
+  for(var a in imgData){
+    if(imgData[a].src == data){
+      return Session.set("choiceImg",imgData[a].Img);
+    }
+  }
+}
+
+function searchImg(){
+  var thumNaill = [];
+  var data = daumService.daumImage.channel.item;
+  //console.log(data);
+  for(var a in data){
+    thumNaill.push({src : data[a].thumbnail, Img : data[a].image});
+  }
+  Session.set("thumNail",thumNaill);
+}
 
 //side 메뉴 애니메이션
 function sideMenueAnimation(){
